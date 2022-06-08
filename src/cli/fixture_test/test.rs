03f057d1e1a8f62cf52bf08a12e3ed3c0f2e9754
Dewyer <dewyerer@gmail.com>
2022-06-09 01:03:20 +0200

    feat: chargeback negative

--- src/cli/fixture_test/test.rs
+++ src/cli/fixture_test/test.rs
@@ -29,6 +29,8 @@ pub fn initialize() {
 #[case("12_chb_simple")]
 #[case("13_chb_no_double")]
 #[case("14_chb_locked_acc")]
+#[case("15_dis_withdrawn")]
+#[case("16_chb_withdrawn")]
 #[tokio::test]
 async fn fixture_test(#[case] file_name: &str) {
     initialize();
